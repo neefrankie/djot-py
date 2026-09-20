1564360cb76bc53951445c93a396f2731c2d7b67
Victor Nee <neefrankie@gmail.com>
2026-09-20 23:06:20 +0800

    feat(inline): add LessthanMatcher for autolink parsing
    Add new LessthanMatcher class to handle autolink parsing for emails
    and URLs. The matcher identifies email addresses and URLs within
    angle brackets (<>) and generates appropriate enter/exit events with
    corresponding content as leaf nodes.

---
+++ src/pyjot/inline/lessthan.py
@@ -0,0 +1,45 @@
+import re
+from typing import Optional
+
+from ..common import (
+    Range,
+)
+from ..event import (
+    Event,
+    ContainerKind,
+    LeafKind,
+)
+from .matcher import Matcher
+from .state import InlineState
+
+_RE_EMAIL = re.compile(r'[^:]@')
+_RE_URL = re.compile(r'[a-zA-Z]:/')
+
+def is_email(s: str) -> bool:
+    return _RE_EMAIL.search(s) is not None
+
+def is_url(s: str) -> bool:
+    return _RE_URL.search(s) is not None
+
+class LessthanMatcher(Matcher):
+
+    def __call__(self, state: InlineState, pos: int, endpos: int) -> Optional[int]:
+        m = state.cursor.find_autolink(pos, endpos)
+        if m is None:
+            return None
+
+        starturl = m.start
+        endurl = m.end
+        url = m.captures[0]
+        if is_email(url):
+            state.events.append(Event.enter(Range(starturl, starturl), ContainerKind.EMAIL)) # <
+            state.events.append(Event.leaf(Range(starturl+1, endurl-1), LeafKind.STR)) # email
+            state.events.append(Event.exit(Range(endurl, endurl), ContainerKind.EMAIL)) # >
+            return endurl+1 # after  >
+        elif is_url(url):
+            state.events.append(Event.enter(Range(starturl, starturl), ContainerKind.URL)) # <
+            state.events.append(Event.leaf(Range(starturl+1, endurl-1), LeafKind.STR)) # url
+            state.events.append(Event.exit(Range(endurl, endurl), ContainerKind.URL)) # >
+            return endurl+1 # after >
+        
+        return None
