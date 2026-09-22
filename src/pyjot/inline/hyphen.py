056d6690c66993d994b695321598bc181c8fd5cf
Victor Nee <neefrankie@gmail.com>
2026-09-22 09:08:17 +0800

    ``` refactor(inline): update HyphenMatcher to use BetweenMatcher base class
    - Replace custom implementation with BetweenMatcher inheritance
    - Update imports to use InlineContainer and InlineLeaf from pyjot.event
    - Change LeafKind references to InlineLeaf for consistency
    - Add proper constructor initialization with container_kind and fallback_leaf
    - Move has_brace check to can_open method override
    - Use super() calls for parent class functionality

--- src/pyjot/inline/hyphen.py
+++ src/pyjot/inline/hyphen.py
@@ -1,25 +1,34 @@
+from pyjot.event import InlineContainer, InlineLeaf
+from pyjot.input import InputText
+
 from ..common import (
     Range,
 )
 from ..event import (
     Event,
-    LeafKind,
+    InlineLeaf,
 )
-from .matcher import Matcher
 from .state import InlineState
+from .between import BetweenMatcher
 
-class HyphenMatcher(Matcher):
+class HyphenMatcher(BetweenMatcher):
+
+    def __init__(self):
+        super().__init__(
+            ch='-', 
+            container_kind=InlineContainer.DELETE, 
+            fallback_leaf=InlineLeaf.STR,
+        )
+
+    def can_open(self, cursor: InputText, pos: int) -> bool:
+        return cursor.has_brace(pos)
 
     def __call__(self, state: InlineState, pos: int, endpos: int) -> int | None:
 
         # {-delete-}
         if state.cursor.is_left_brace(pos-1) or state.cursor.is_right_brace(pos+1): # {- or -}
-            newpos = make_between_matched(
-                c='-',
-                annotation='delete',
-                defaultmatch='str',
-                opentest=has_brace,
-            )(self, pos, endpos)
+            newpos = super().__call__(state, pos, endpos)
+            
             if newpos:
                 return newpos
             
@@ -34,7 +43,7 @@ class HyphenMatcher(Matcher):
             hyphens -= 1 # last hyphen is close del
 
         if hyphens == 0: # this means we have '-}'
-            state.events.append(Event.leaf(Range(pos, pos+1), LeafKind.STR))
+            state.events.append(Event.leaf(Range(pos, pos+1), InlineLeaf.STR))
             return pos+2
         
         # Try to contruct a homogeneous sequence of dashes
@@ -44,23 +53,43 @@ class HyphenMatcher(Matcher):
         all_en = hyphens % 2 == 0
         while hyphens > 0:
             if all_em:
-                state.events.append(Event.leaf(Range(pos, pos+2), LeafKind.EM_DASH))
+                state.events.append(
+                    Event.leaf(
+                        Range(pos, pos+2),
+                        InlineLeaf.EM_DASH
+                    )
+                )
                 pos = pos + 3
                 hyphens = hyphens - 3
             elif all_en:
-                state.events.append(Event.leaf(Range(pos, pos+1), LeafKind.EN_DASH))
+                state.events.append(
+                    Event.leaf(
+                        Range(pos, pos+1),
+                        InlineLeaf.EN_DASH
+                    )
+                )
                 pos = pos + 2
                 hyphens = hyphens - 2
             elif hyphens >= 3 and (hyphens % 2 != 0 or hyphens > 4): # odd number of dashes
-                state.events.append(Event.leaf(Range(pos, pos+2), LeafKind.EM_DASH))
+                state.events.append(
+                    Event.leaf(
+                        Range(pos, pos+2),
+                        InlineLeaf.EM_DASH
+                    )
+                )
                 pos = pos + 3
                 hyphens = hyphens - 3
             elif hyphens >= 2:
-                state.events.append(Event.leaf(Range(pos, pos+1), LeafKind.EN_DASH))
+                state.events.append(
+                    Event.leaf(
+                        Range(pos, pos+1),
+                        InlineLeaf.EN_DASH
+                    ),
+                )
                 pos = pos + 2
                 hyphens = hyphens - 2
             else:
-                state.events.append(Event.leaf(span=Range(pos, pos), kind=LeafKind.STR))
+                state.events.append(Event.leaf(span=Range(pos, pos), kind=InlineLeaf.STR))
                 pos = pos + 1
                 hyphens = hyphens - 1
         
