c097fa8a1af6ac9208535851fb353b98e8ee4ffa
Victor Nee <neefrankie@gmail.com>
2026-09-26 19:31:32 +0800

    fix(djot): handle multiple opening parentheses in link mode

--- src/djot/inline/paren_left.py
+++ src/djot/inline/paren_left.py
@@ -13,8 +13,12 @@ from .state import InlineState
 class LeftParenMatcher(Matcher):
 
     def __call__(self, state: InlineState, pos: int, endpos: int) -> Optional[int]:
-        # (
+        # If we are not entering link mode, this is a plain text.
         if not state.destination:
             return None
+
+        # If we are already in link mode, the opening paren is
+        # aleady consumed by RightBracketMatcher.
+        # So if we see another opening paren, it should be treated as plain text.
         state.add_opener('(', Event.leaf(Range(pos, pos), InlineLeaf.STR))
         return pos+1
